f4ad6d90082e2c5fe40338a07119a159afda6b00
Pablo MD <pablo.martinez.dovale@gmail.com>
2025-11-02 22:15:27 +0100

    Fixes 3

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { accountRouter } from "./routers/account";
 
@@ -8,7 +7,6 @@ import { accountRouter } from "./routers/account";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-	post: postRouter,
     account: accountRouter,
 });
 
